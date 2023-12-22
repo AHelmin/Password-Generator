64e1b3bed63d23a4b7b90ca0b53114d5e92bf388
helm0127 <helm0127@morris.umn.edu>
2023-12-22 15:17:27 -0800

    removed pseudocode comments, commented on code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,97 +13,53 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// var allLowerCase = ['a','b','c'//all the way to z]
-
-//prompt(), alert(), confirm() 
-
-//will need random number generator or possibly two for the addition of lower case
-// var randomNumber = Math.floor(Math.random() * Max);
-
-//will need to create arrays of letters, numbers, and special characters
-// var letterString = 'abcdefghijklmnopqrstuvwxyz'
-// var capitalLetterString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-// var numberString = '0123456789'
-// var specialCharString = '`~!@#$%^&*()-_+={}[]|\;:"<>/?'
-
-// var letterArray = Array.from(letterString);
-// console.log(letterArray);
-// var capitalLetterArray = Array.from(capitalLetterString);
-// console.log(capitalLetterArray);
-// var numberArray = Array.from(numberString);
-// console.log(numberArray);
-// var specialCharArray = Array.from(specialCharString);
-// console.log(specialCharArray);
-
-//will need to ask the length of pass
-//will need to as if lower case letters
-// length of pass will be used as the iteration length
-//var userLengthInput = completed by the user
-//will need to construct two arrays one with lower case and one with not
-//will also need two random nuber gens for each length
-
-// var capitalOnlyArray = capitalLetterArray.concat(numberArray, specialCharArray);
-// console.log(capitalOnlyArray);
-// var lowerCasePlusAllArray = letterArray.concat(capitalLetterArray, numberArray, specialCharArray);
-// console.log(lowerCasePlusAllArray);
-
 function generatePassword() {
+  //creates strings of character options
   var letterString = 'abcdefghijklmnopqrstuvwxyz'
   var capitalLetterString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   var numberString = '0123456789'
   var specialCharString = '`~!@#$%^&*()-_+={}[]|\;:"<>/?'
-  // var letterArray = Array.from(letterString);
-  // var capitalLetterArray = Array.from(capitalLetterString);
-  // var numberArray = Array.from(numberString);
-  // var specialCharArray = Array.from(specialCharString);
-  // var userChoiceArr = []
   var userChoiceStr = ''
-
+//prompts user to input a password length, second line converts string to number
   var lengthInput = prompt('How many characters would you like the password to be?');
   var passLength = parseInt(lengthInput)
-
+//checks for conditions, if no pass then alert to put in a number within the length requuirement and sends back to beginning of function
   if (!passLength || passLength === 0 || !Number.isInteger(passLength) || passLength < 8 || passLength > 128) {
     alert('Please enter a number between 8 and 128 characters.');
     return generatePassword() 
   }
-
+//prompts user to choose what type of characters they would like
   var lowerCase = confirm('Would you like to use lower case letters?');
   var upperCase = confirm('Would you like to use upper case letters?');
   var numbers = confirm('Would you like to use numbers?');
   var specialCharacters = confirm('Would you like to use special characters?');
 
-
+//checks to make sure that at least one of the characters were chosen, if not alerts to choose one type and sends back to start
   if (lowerCase === false && upperCase === false && numbers === false && specialCharacters === false) {
-    alert('Please enter a character type.');
+    alert('Please choose at least one character type.');
     return generatePassword()
   }
+  //builds the full password strings based on user choice
   if (lowerCase) {
-    // userChoiceArr = userChoiceArr.concat(letterArray);
     userChoiceStr += letterString
   }
   if (upperCase) {
-    // userChoiceArr = userChoiceArr.concat(capitalLetterArray);
     userChoiceStr += capitalLetterString
   }
   if (numbers) {
-    // userChoiceArr = userChoiceArr.concat(numberArray);
     userChoiceStr += numberString
   }
   if (specialCharacters) {
-    // userChoiceArr = userChoiceArr.concat(specialCharArray);
     userChoiceStr += specialCharString
   }
+  //builds the password with the help of random number generation
   console.log(passLength)
   console.log(userChoiceStr)
   var passString = ''
   for (var i = 0; i < passLength; i++) {
     var randomNumber = Math.floor(Math.random() * userChoiceStr.length);
-    console.log(i);
-    console.log(randomNumber);
-    console.log(userChoiceStr[randomNumber]);
     passString += userChoiceStr[randomNumber];
   }
-  console.log(passString)
   return passString
 
 }
